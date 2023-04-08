89689c7bdef9f051726e810b07a646d3cce8d519
Bernard <bmwinyoglee@gmail.com>
2023-04-08 10:56:09 +0000

    Recursion first commit after several test and suffering

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Print a string
+ * @s: string
+ * bravo-dev
+ */
+
+void _puts_recursion(char *s)
+{	
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,4 @@
+# 0x07. C - Recursion
+-	Task 0 : Writing a function that prints a string, followed by a new line.
+-	Task 1 : Writing a function that prints a string in reverse.
+-	Task 2 : Writing a function that returns the length of a string.
