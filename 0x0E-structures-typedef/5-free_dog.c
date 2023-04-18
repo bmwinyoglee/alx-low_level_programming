0cd5e4e45cf9945d3550a515423dbe34d1209bd5
Bernard <bmwinyoglee@gmail.com>
2023-04-18 08:56:37 +0000

    Struct TypeDef

---
+++ 0x0E-structures-typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free the dogs
+ * @d: pointer to dog struct
+ * bravo-dev
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+
+		free(d);
+	}
+}
