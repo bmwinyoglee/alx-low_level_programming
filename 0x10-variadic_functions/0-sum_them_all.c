bda4ba93437a80eaa3cdbc8a3fd80eb55b1dc4ad
Bernard <bmwinyoglee@gmail.com>
2023-04-20 21:59:40 -0700

    update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,18 +4,25 @@
  * sum_them_all - return the sum of all its parameters
  * @n: number of extra params
  * Return: total sum
+ * bravo-dev
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
-	va_list list;
+	// creating va_list to store the variable argument list
+	va_list my_nums;
+	unsigned int count;
 	int sum;
 
-	va_start(list, n);
-	for (i = 0, sum = 0; i < n; i++)
-		sum += va_arg(list, int);
+	if (n == 0)
+		return (0);
+	sum = 0;
 
-	va_end(list);
+	// init valist for the number of arguments
+	va_start(my_nums, n);
 
+	// loop through/access all arguments stored in the valist
+	for (count = 0; count < n; count++)
+		sum += va_arg(my_nums, int);
+	va_end(my_nums);
 	return (sum);
 }
