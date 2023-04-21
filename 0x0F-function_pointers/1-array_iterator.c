b6105ee8179797d4c561ac0d3ee1b12ce2386d1c
Bernard <bmwinyoglee@gmail.com>
2023-04-21 05:24:25 -0700

    Make changes to task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,13 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - execute the function in param to array
- * 
- * @array: array we work on
- * @size: size of array
- * @action: the action we do
+ * array_iterator - a function given as a
+ * parameter on each element of an array.
  *
- * Return hwee (nothing)
+ * @array: array to execute func on
+ * @size: is the size of the array
+ * @action:  is a pointer to the function you need to use
+ *
+ * Return: nothing
  * bravo-dev
  */
 
@@ -15,6 +16,13 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; 1 < size; i++;)
-		action(array[i]);
+	if (array && action)
+	{
+		i = 0;
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
 }
