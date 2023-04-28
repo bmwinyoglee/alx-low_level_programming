b2b7c83982d83c6f26745318b143fa5067465f14
bravo <bmwinyoglee@gmail.com>
2023-04-28 05:17:53 +0000

    bug fixes task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,23 +1,20 @@
 #include "lists.h"
-
-
 /**
- * listint_len - ...
- *
- * @h: ...
- * Return: ...
+ * list_len - returns then number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ * brav0-dev
  */
-size_t listint_len(const listint_t *h)
-{
-const listint_t *temp = h;
-size_t count = 0;
 
-if (h == NULL)
-return (0);
-while (temp != NULL)
+size_t list_len(const list_t *h)
 {
-count++;
-temp = temp->next;
-}
-return (count);
+	size_t number_of_nodes;
+
+	number_of_nodes = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		number_of_nodes++;
+	}
+	return (number_of_nodes);
 }
