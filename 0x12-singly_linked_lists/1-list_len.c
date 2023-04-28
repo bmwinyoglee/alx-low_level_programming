d9d48e003aff7b145b22dfd2ee1f930e01417853
bravo <bmwinyoglee@gmail.com>
2023-04-28 05:23:27 +0000

    bug fixes task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
  * list_len - returns then number of elements in a list.
  * @h: singly linked list.
  * Return: number of elements in the list.
- * brav0-dev
+ * bravo-dev
  */
 
 size_t list_len(const list_t *h)
