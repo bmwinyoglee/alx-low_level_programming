c86075e271b4edcb7870a75d1d2af7f1a86eb2ec
Bravo10 <bmwinyoglee@gmail.com>
2023-05-05 20:30:24 +0000

    bug fix

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -4,9 +4,10 @@
 /**
  * find_listint_loop - find the loop in a linked list
  * @head: head of linked list
- * Description: Not allowed to use malloc, free or arrays.
- * Can only declare a max of 2 variables.
+ * Description: cannot use malloc, free or arrays.
+ * a max of 2 variables.
  * Return: Address of node where loop starts, or NULL if no loop found.
+ * bravo-dev
  */
 listint_t *find_listint_loop(listint_t *head)
 {
